511573441c687cd82f0f8d954011a71947155024
Alexandre St-Onge <stonge.alex@gmail.com>
2016-04-03 22:43:26 -0400

    first commit

---
+++ NativeLibExample/NativeLibExample/NativeLibExample.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace NativeLibExample
+{
+    //Declare with C linkage to avoid name mangling
+    extern "C"
+    {
+        void test(unsigned char* inputImage, float* output);
+    }
+}
